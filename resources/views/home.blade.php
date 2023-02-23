f05c1563df54d10464c38f9dceece18588081233
antonionovelli92 <anto.novelli@hotmail.it>
2023-02-23 16:46:15 +0100

    add img and ul

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,6 +7,23 @@
     <title>DC Comics | Home </title>
 </head>
 <body>
-    <h1>Home</h1>
+    <header>
+        <figure>
+            <a href="{{url('/')}}">
+                <img src="{{asset('images/dc-logo.png')}}" alt="logo DC">
+            </a>
+        </figure>
+        <ul>
+            <li><a href="{{route('characters')}}">CHARACTERS</a></li>
+            <li><a href="{{route('comics')}}">COMICS</a></li>
+            <li><a href="{{route('movies')}}">MOVIES</a></li>
+            <li><a href="{{route('tv')}}">TV</a></li>
+            <li><a href="{{route('games')}}">GAMES</a></li>
+            <li><a href="{{route('collectibles')}}">COLLECTIBLES</a></li>
+            <li><a href="{{route('videos')}}">VIDEOS</a></li>
+            <li><a href="{{route('fans')}}">FANS</a></li>
+            <li><a href="{{route('news')}}">NEWS</a></li>
+        </ul>
+    </header>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,47 @@ Route::get('/', function () {
     return view('home');
 })->name('home');
 
+
 Route::get('/comics', function () {
     return view('comics');
 })->name('comics');
 
+
 Route::get('/movies', function () {
     return view('movies');
 })->name('movies');
+
+
+Route::get('/games', function () {
+    return view('games');
+})->name('games');
+
+
+Route::get('/fans', function () {
+    return view('fans');
+})->name('fans');
+
+
+Route::get('/characters', function () {
+    return view('characters');
+})->name('characters');
+
+
+Route::get('/collectibles', function () {
+    return view('collectibles');
+})->name('collectibles');
+
+
+Route::get('/news', function () {
+    return view('news');
+})->name('news');
+
+
+Route::get('/tv', function () {
+    return view('tv');
+})->name('tv');
+
+
+Route::get('/videos', function () {
+    return view('videos');
+})->name('videos');
